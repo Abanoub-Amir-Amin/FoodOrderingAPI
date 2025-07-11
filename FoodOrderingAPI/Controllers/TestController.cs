04cba62530257dcdae6c0e03281d1cf7a443ff0c
Abanoub-Amir-Amin <abanoubamir288@gmail.com>
2025-07-11 18:03:09 +0300

    Add project files.

---
+++ FoodOrderingAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrderingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Test endpoint is working!");
+        }
+    }
+}
