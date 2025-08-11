5cf687152503d6ea7ec9ee58d955e56907da870d
KHOLUD AHMED <kholudahmed666@gmail.com>
2025-08-11 10:14:15 +0300

    update after edit profile dto

--- FoodOrderingAPI/Program.cs
+++ FoodOrderingAPI/Program.cs
@@ -167,15 +167,15 @@ namespace FoodOrderingAPI
 
             // Register OpenAPI/Swagger services
             builder.Services.AddOpenApi();
-            builder.Services.AddCors(op =>
+            // Add CORS policy to allow Angular frontend
+            builder.Services.AddCors(options =>
             {
-                op.AddPolicy("public", builder => builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetIsOriginAllowed(_ => true));
-                //op.AddPolicy("subscription", policy =>
-                //{
-                //    policy.WithOrigins("127.0.0.1").WithHeaders("token", "role").WithMethods("get");
-                //});
+                options.AddPolicy("AllowAngularDevClient", policy =>
+                    policy.WithOrigins("http://localhost:4200", "http://localhost:62532") // ✅ ضع هنا الـ Origin الذي يعمل عليه Angular
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials());
             });
-
 
             // It configures EF Core to use SQL Server as the database provider,
             // and enables support for spatial data types (like geography, geometry) using NetTopologySuite.
@@ -212,7 +212,7 @@ namespace FoodOrderingAPI
             app.UseStaticFiles();
 
             //app.UseHttpsRedirection();
-            app.UseCors("public");
+            app.UseCors("AllowAngularDevClient");
 
             app.UseAuthentication();
             app.UseAuthorization();
