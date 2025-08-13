633787bd0828701d4ac2566ed771e0a0c1838a06
Abanoub-Amir-Amin <abanoubamir288@gmail.com>
2025-08-13 16:47:53 +0300

    Reveiw Mod.

--- FoodOrderingAPI/DTO/ReviewGetterDTO.cs
+++ FoodOrderingAPI/DTO/ReviewGetterDTO.cs
@@ -1,10 +1,12 @@
 ﻿namespace FoodOrderingAPI.DTO
 {
-    public class ReviewDto
+    public class ReviewGetterDTO
     {
+        public Guid ReviewID { get; set; }
         public Guid OrderID { get; set; }
-        public string CustomerID { get; set; } 
-        public string RestaurantID { get; set; }  
+        public string CustomerID { get; set; }
+        public string UserName { get; set; }
+        public string RestaurantID { get; set; }
         public float Rating { get; set; }
         public string Comment { get; set; }
         public DateTime CreatedAt { get; set; }
--- FoodOrderingAPI/Services/IReviewService.cs
+++ FoodOrderingAPI/Services/IReviewService.cs
@@ -5,11 +5,11 @@ namespace FoodOrderingAPI.Services
 {
     public interface IReviewService
     {
-        Task<IEnumerable<Review>> GetAllReviewsAsync();
-        Task<Review> GetReviewByIdAsync(Guid reviewId);
-        Task<IEnumerable<Review>> GetReviewsByOrderIdAsync(Guid orderId);
-        Task<IEnumerable<Review>> GetReviewsByCustomerIdAsync(string customerId);
-        Task<IEnumerable<Review>> GetReviewsByRestaurantIdAsync(string restaurantId);
+        Task<IEnumerable<ReviewGetterDTO>> GetAllReviewsAsync();
+        Task<ReviewGetterDTO> GetReviewByIdAsync(Guid reviewId);
+        Task<IEnumerable<ReviewGetterDTO>> GetReviewsByOrderIdAsync(Guid orderId);
+        Task<IEnumerable<ReviewGetterDTO>> GetReviewsByCustomerIdAsync(string customerId);
+        Task<IEnumerable<ReviewGetterDTO>> GetReviewsByRestaurantIdAsync(string restaurantId);
         Task CreateReviewAsync(ReviewDto review);
         Task<bool> DeleteReviewAsync(Guid reviewId);
     }
